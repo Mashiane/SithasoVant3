e7e9f8f7d1af4336cd7dc30cf294d6b029910524
Mashy <mbanga.anele@gmail.com>
2022-04-10 02:44:10 +0200

    Version 1.00

--- sithasovant3demo.php
+++ sithasovant3demo.php
@@ -1,4 +1,21 @@
 <?php $rest_json = file_get_contents("php://input");$_POST = json_decode($rest_json, true);$request='';if(isset($_POST['request'])){$request = $_POST['request'];$params = $_POST['params'];}if (!function_exists($request)) die("invalid request: '" . $request . "'"); 
+$key = 'qkwjdiw239&&jdafweihbrhnan&^%$ggdnawhd4njshjwuuO'; 
+	 
+//ENCRYPT FUNCTION 
+function encryptthis($data, $key) { 
+	$encryption_key = base64_decode($key); 
+	$iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('aes-256-cbc')); 
+	$encrypted = openssl_encrypt($data, 'aes-256-cbc', $encryption_key, 0, $iv); 
+	return base64_encode($encrypted . '::' . $iv); 
+} 
+ 
+//DECRYPT FUNCTION 
+function decryptthis($data, $key) { 
+	$encryption_key = base64_decode($key); 
+	list($encrypted_data, $iv) = array_pad(explode('::', base64_decode($data), 2),2,null); 
+	return openssl_decrypt($encrypted_data, 'aes-256-cbc', $encryption_key, 0, $iv); 
+} 
+ 
 function EmailSend($from, $to, $cc, $subject, $msg) { 
 $hdr  = 'MIME-Version: 1.0' . "\r\n"; 
 $hdr .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n"; 
